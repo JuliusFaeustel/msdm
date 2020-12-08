4a2c3cf61678d26a875daaf09b1107c2b53f9bf1
Clemens Kujus <clemens.kujus@htw-dresden.de>
2020-12-08 19:36:48 +0100

    Work on new structure

--- key-value-store/newStructure/newTakt1.py
+++ key-value-store/newStructure/newTakt1.py
@@ -4,6 +4,21 @@ import csv
 #decode_responses, da die Rueckgabewerte von z.B. r.get(...) sonst als 'binaer' codiert
 r = redis.Redis(decode_responses=True)
 
+recDateBeg = datetime(1989, 1, 9, 12, 0, 0)
+recDateEnd = datetime(1989, 1, 9, 12, 0, 0)
+minDateEnd = datetime(2010, 1, 9, 12, 0, 0)
+zerDiff = recDateEnd - recDateBeg
+maxDate = minDateEnd - recDateBeg
+
+allCons = r.lrange("con",0,-1)
+allConLen = len(allCons)
+
+allTeil = r.smembers("TEIL")
+allFa = r.smembers("FA")
+
+writer = ""
+diffFile = open("takt1.txt", "w")
+
 lua = """
 local offset = tonumber(ARGV[1]);
 local limit = tonumber(ARGV[2]);
@@ -55,7 +70,83 @@ return ids;
 
 myLua = r.register_script(lua)
 
-result = myLua(keys=['FA:009004'],args=[1,200])
+for teil in allTeil:
+    diffFile.write(teil + "\n")
+    for fa in allFa:
+        r.bitop("AND","opCon",teil,fa)
+        result = myLua(keys=['opCon'],args=[1,allConLen])
+        minDiff = maxDate.total_seconds()
+        maxDiff = zerDiff.total_seconds()
+        avgCounter = 0
+        avgTime = 0
+        allRejects = 0
+        minRejects = 999
+        maxRejects = 0
+        ausCounter = 0
+        rejects = 0
+        menge = 0
+        snrDict = {}
+        ausDict = {}
+        
+        if result:
+            for res in result:
+                con = allCons[res-1]
+                localRec = zerDiff
+                smallerOneH = 0
+                conSplit = con.split(":")
+                beginDateStamp = float(conSplit[0])
+                endDateStamp = float(conSplit[2])
+                snr = conSplit[3]
+
+                if not beginDateStamp == endDateStamp:
+
+                    if not snr in snrDict:
+                        snrDict[snr] = 0
+                    
+                    diffTimeStamp = float(endDateStamp - beginDateStamp)
+
+                    smallerOneH = 0
+                    lastRec = maxDiff
+
+                    if diffTimeStamp <= 3600:
+                        smallerOneH = 1
+                        if diffTimeStamp < minDiff:
+                            minDiff = diffTimeStamp
+
+                        if diffTimeStamp > maxDiff:
+                            maxDiff = diffTimeStamp
+
+                        avgTime = avgTime + diffTimeStamp
+                        avgCounter = avgCounter + 1
+
+                    snrDict[snr] = snrDict[snr] + 1
+
+                if not snr in ausDict:
+                    ausDict[snr] = 0          
+                ausDict[snr] = ausDict[snr] + 1
+            
+            menge = len(snrDict)
+            
+            for k,v in ausDict.items():
+                rejects = v - 1
+                if rejects > maxRejects:
+                    maxRejects = rejects
+
+                if rejects < minRejects and rejects >= 0:
+                    minRejects = rejects
+
+                if rejects >= 0:
+                    allRejects = allRejects + rejects
+
+            if avgCounter > 0:
+                avgGesTime = avgTime/avgCounter
+                avgGesReject = allRejects/menge
+            else:
+                avgGesTime = 0
+                avgGesReject = 0
+            
+            writer = fa+";"+str(menge)+";"+str(minDiff)+";"+str(maxDiff)+";"+str(avgGesTime)+";"+str(minRejects)+";"+str(maxRejects)+";"+str(avgGesReject)+"\n"
+            diffFile.write(writer)
 
 
 
