ba0241b376a7a4c62c38c344b7111073a2c10922
Paul Kristian Picht <s78846@htw-dresden.de>
2021-01-18 14:31:07 +0100

    Inital RIP

--- backendWeb/AuswertungCSV/delimiterchange.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-input = pd.read_csv("takt4.txt", delimiter=r"\s+", error_bad_lines=False)
-print(input)
-
-for line in input:
-    print(line[1]+","+line[2]+" "+line[3]+","+line[4])
